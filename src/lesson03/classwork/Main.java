56d26e97e4c1677f8574e19dd7e2c4a555fffef5
OliaStfn <kardash44@mail.ru>
2017-02-19 21:34:52 +0200

    Initial commit

---
+++ src/lesson03/classwork/Main.java
@@ -0,0 +1,20 @@
+package lesson03.classwork;
+
+public class Main {
+    public static void main(String[] args) {
+        Human obj = new Human();
+        Human object = new Human("John", "Brown");
+        obj.setBornDay(22);
+        obj.setBornMonth(3);
+        obj.setBornYear(1999);
+        obj.setName("Olia");
+        obj.setSurname("Stefanyshyn");
+        int day = object.getBornDay();
+        int month = object.getBornMonth();
+        int year = object.getBornYear();
+        String  name= object.getName();
+        String  surname= object.getSurname();
+
+        obj.showInfo();
+    }
+}
